0e2c18fa0012b1a68454ffced28bc7f8c3a593fc
Ricardo Momoi <shinjimomoi@gmail.com>
2022-06-09 22:24:51 +0900

    controllers added

---
+++ app/controllers/player_skills_controller.rb
@@ -0,0 +1,2 @@
+class PlayerSkillsController < ApplicationController
+end
---
+++ test/controllers/player_skills_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PlayerSkillsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
